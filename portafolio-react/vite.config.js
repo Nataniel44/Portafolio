7382af0c7a8191da36ca491aeb5a37c4153d7044
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-07-24 13:20:40 -0300

    ajustes3

--- portafolio-react/vite.config.js
+++ portafolio-react/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/Nataniel44/Portafolio",
+  base: "https://nataniel44.github.io/Portafolio/",
 });
